279dafb1d5b9f6d2d24f6601fd27ba17e60900d5
Anna <ospostpost9@gmail.com>
2023-09-18 10:27:28 +0100

    Lesson 1.4

--- sandbox/src/main/java/Geometry.java
+++ sandbox/src/main/java/Geometry.java
@@ -1,6 +1,28 @@
 public class Geometry {
     public static void main(String[] args) {
-        var side = 7.0;
-        System.out.println("Площадь квадрата cо cтороной " + side + " = " + (side*side));
+        printSquareArea(7.0);
+        printSquareArea(5.0);
+        printSquareArea(3.0);
+        printRectangleArea(3.0, 5.0);
+        printRectangleArea(7., 8.);
+    }
+
+    private static void printRectangleArea(double a, double b) {
+        System.out.println("Площадь прмоугольника cо cторонами " + a + " и " + b + " = " + rectangleArea(a, b));
+    }
+
+    private static double rectangleArea(double a, double b) {
+        return a * b;
+    }
+
+    // void - ничего не возращает функци
+    // static - функци глобальна
+    static void printSquareArea(double side) {
+        System.out.println("Площадь квадрата cо cтороной " + side + " = " + squareArea(side));
+    }
+
+    // double в функции указываем тип возращаемого результата (выражени)
+    private static double squareArea(double a) {
+        return a * a;
     }
 }
