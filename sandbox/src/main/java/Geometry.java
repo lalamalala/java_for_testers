4767e85947b32f442152ba379bbbe902fcd386a0
Anna <ospostpost9@gmail.com>
2023-09-18 09:49:11 +0100

    lesson 1.1-1.3

---
+++ sandbox/src/main/java/Geometry.java
@@ -0,0 +1,6 @@
+public class Geometry {
+    public static void main(String[] args) {
+        var side = 7.0;
+        System.out.println("Площадь квадрата cо cтороной " + side + " = " + (side*side));
+    }
+}
