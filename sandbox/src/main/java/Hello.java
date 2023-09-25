5a61c618ca6339182c5dfbc26e3599407845abc4
Anna <ospostpost9@gmail.com>
2023-09-25 14:34:19 +0200

    Лекция 2.1. Исключения

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -2,7 +2,18 @@ import java.io.File;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello,world");
+        try {
+            int z = calculate();
+            System.out.println(z);
+            System.out.println("Hello,world");
+        } catch (ArithmeticException exception) {
+           // System.out.println(exception.getMessage());
+            exception.printStackTrace();
+        }
+
+
+
+
  /*       System.out.println(2 + 2);
         System.out.println(2 -2);
         System.out.println(2 * 2);
@@ -11,12 +22,24 @@ public class Hello {
         System.out.println("Hello" + " world");
         System.out.println("2+2= "+4);
         System.out.println("2+2= "+2+2);
-        System.out.println("2+2= "+(2+2)); */
+        System.out.println("2+2= "+(2+2));
 
-   //     var configFile = "build.gradle"; // path to file
+       var configFile = "build.gradle"; // path to file
         var configFile = new File("sandbox/build.gradle");
         System.out.println(configFile.getAbsolutePath());
-        System.out.println(configFile.exists());
+        System.out.println(configFile.exists()); */
 
+    }
+
+    private static int calculate() {
+        var x = 1;
+        var y = 1;
+        int z = divide(x, y);
+        return z;
+    }
+
+    private static int divide(int x, int y) {
+        var z = x / y;
+        return z;
     }
 }
