f52206f56f32488e169632ddf34f7ceef38f7a3c
Anna <ospostpost9@gmail.com>
2023-09-25 22:47:59 +0200

    Лекция 2.4. Сравнение объектов (метод equals)

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -1,5 +1,7 @@
 package ru.stqa.geometry.figures;
 
+import java.util.Objects;
+
 public record Rectangle (double a, double b) {
  public Rectangle {
      if (a < 0 || b < 0) {
@@ -7,6 +9,21 @@ public record Rectangle (double a, double b) {
      }
 
  }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return (Double.compare(rectangle.a, this.a) == 0 && Double.compare(rectangle.b, this.b) == 0)
+                || (Double.compare(rectangle.a, this.b) == 0 && Double.compare(rectangle.b, this.a) == 0);
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
+
     public static void printRectangleArea(double a, double b) {
         var text = String.format("Area of a rectangle with sides %f and %f = %f", a, b, rectangleArea(a, b));
         System.out.println(text);
