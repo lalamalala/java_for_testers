bcc23c7cfd211da0f3708ef804003063875e56aa
Anna <ospostpost9@gmail.com>
2023-10-07 23:17:58 +0200

    Задание №9: Параметризовать тесты для создания групп и контактов

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -1,57 +1,54 @@
 package tests;
 
 import model.ContactData;
-import model.GroupData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactCreationTests extends TestBase{
 
-    @Test
-    public void canCreateContact() {
+    public static List<ContactData> contactProvider() {
+        var result = new ArrayList<ContactData>();
+        for (var firstname : List.of("","first name")) {
+            for (var middlename : List.of("","middle name")) {
+                for (var lastname : List.of("", "last name")) {
+                    result.add(new ContactData(firstname, middlename, lastname));
+                }
+            }
+        }
+        for (int i = 0; i < 5 ; i++) {
+            result.add(new ContactData(randomString(i * 10), randomString(i * 10), randomString(i * 10)));
+        }
+        return result;
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("contactProvider")
+    public void canCreateMultipleContacts(ContactData contact) {
         int contactCount = app.contacts().getCount();
-        app.contacts().openCreateContactPage();
-        app.contacts().createContact(new ContactData("Lala", "Byby", "GUG"));
+        app.contacts().createContact(contact);
         int newContactCount = app.contacts().getCount();
         Assertions.assertEquals(contactCount + 1, newContactCount);
+
     }
 
-    @Test
-    public void canCreateMultipleContacts() {
-        int n = 5;
+    public static List<ContactData> negativeContactProvider() {
+        var result = new ArrayList<ContactData>(List.of(
+                new ContactData("first name'", "sfsfds","sdfdsf")));
+        return result;
+    }
+
+    @ParameterizedTest
+    @MethodSource("negativeContactProvider")
+    public void canNotCreateContact(ContactData contact) {
         int contactCount = app.contacts().getCount();
-
-        for (int i = 0; i < n ; i++) {
-            app.contacts().openCreateContactPage();
-            app.contacts().createContact(new ContactData(randomString(i), "Byby", "GUG"));
-
-        }
-
+        app.contacts().createContact(contact);
         int newContactCount = app.contacts().getCount();
-        Assertions.assertEquals(contactCount + n, newContactCount);
-
-    }
-
-    @Test
-    public void canCreateGroupWithFirstNameOnly() {
-        app.contacts().openCreateContactPage();
-        var emptyContact = new ContactData();
-        var contactWithFirstName = emptyContact.withFirstName("some name");
-        app.contacts().createContact(contactWithFirstName);
-
-    }
-
-    @Test
-    public void canCreateGroupWithMiddleNameOnly() {
-        app.contacts().openCreateContactPage();
-        app.contacts().createContact(new ContactData().withMiddleName("some name"));
-
-    }
-
-    @Test
-    public void canCreateGroupWithLastNameOnly() {
-        app.contacts().openCreateContactPage();
-        app.contacts().createContact(new ContactData().withLastName("some name"));
-
+        Assertions.assertEquals(contactCount, newContactCount);
     }
 }
