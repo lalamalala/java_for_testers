b4f816661a567996afb042bab5c1fa58e2ce4a47
Anna <ospostpost9@gmail.com>
2023-10-14 14:52:36 +0200

    Лекция 5.2. Список файлов в директории

--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -3,6 +3,8 @@ package tests;
 import manager.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.io.File;
+import java.nio.file.Paths;
 import java.util.Random;
 
 public class TestBase {
@@ -23,11 +25,14 @@ public class TestBase {
         for (int i = 0; i < n; i++) {
             result = result + (char)('a' + rnd.nextInt(26));
         }
-
-     //   if (n < 20) {
-     //       result = result + '\'';
-     //   }
         return result;
+    }
+
+    public static String randomFile(String dir) {
+        var fileNames = new File(dir).list();
+        var rnd = new Random();
+        var index = rnd.nextInt(fileNames.length);
+        return Paths.get(dir,fileNames[index]).toString();
     }
 
 }
