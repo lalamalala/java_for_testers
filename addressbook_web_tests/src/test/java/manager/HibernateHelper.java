0d75ab451e8a96d2761b7d36ece8a1d00878b52a
Anna <ospostpost9@gmail.com>
2023-10-29 09:59:17 +0100

    Лекция 7.3. Функции-трансформеры

--- addressbook_web_tests/src/test/java/manager/HibernateHelper.java
+++ addressbook_web_tests/src/test/java/manager/HibernateHelper.java
@@ -12,6 +12,7 @@ import org.hibernate.cfg.Configuration;
 import java.awt.print.Book;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class HibernateHelper extends HelperBase {
@@ -31,19 +32,17 @@ public class HibernateHelper extends HelperBase {
     }
 
     static  List<GroupData> convertList(List<GroupRecord> records) {
-        List<GroupData> result = new ArrayList<>();
-        for (var record : records) {
-            result.add(convert(record));
-        }
-        return result;
+        return records
+                .stream()
+                .map(HibernateHelper::convert)
+                .collect(Collectors.toList());
     }
 
     static  List<ContactData> convertContactList(List<ContactRecord> records) {
-        List<ContactData> result = new ArrayList<>();
-        for (var record : records) {
-            result.add(convert(record));
-        }
-        return result;
+        return records
+                .stream()
+                .map(HibernateHelper::convert)
+                .collect(Collectors.toList());
     }
 
     private static GroupData convert(GroupRecord record) {
