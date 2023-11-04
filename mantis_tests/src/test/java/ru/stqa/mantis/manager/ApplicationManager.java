2c88362d7f79d2e9a2134ad76609156bcfed4c06
Anna <ospostpost9@gmail.com>
2023-11-04 10:43:24 +0100

    Лекция 8.3. Взаимодействие с сервером по протоколу HTTP

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -13,6 +13,7 @@ public class ApplicationManager {
     private String string;
     private Properties properties;
     private SessionHelper sessionHelper;
+    private HttpSessionHelper httpSessionHelper;
 
     public void init(String browser,Properties properties) {
         this.string = browser;
@@ -44,4 +45,15 @@ public class ApplicationManager {
         }
         return sessionHelper;
         }
+
+    public HttpSessionHelper http() {
+        if (httpSessionHelper == null) {
+            httpSessionHelper = new HttpSessionHelper(this);
+        }
+        return httpSessionHelper;
+    }
+
+    public String property(String name) {
+        return properties.getProperty(name);
+    }
 }
