71e33d96737f64031dfea497e7d306bd9dcd8f69
Anna <ospostpost9@gmail.com>
2023-11-04 11:46:04 +0100

    Лекция 8.4. Почтовый сервер James и управление через интерфейс командной строки

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -14,6 +14,7 @@ public class ApplicationManager {
     private Properties properties;
     private SessionHelper sessionHelper;
     private HttpSessionHelper httpSessionHelper;
+    private JamesCliHelper jamesCliHelper;
 
     public void init(String browser,Properties properties) {
         this.string = browser;
@@ -51,6 +52,13 @@ public class ApplicationManager {
             httpSessionHelper = new HttpSessionHelper(this);
         }
         return httpSessionHelper;
+    }
+
+    public JamesCliHelper jamesCli() {
+        if (jamesCliHelper == null) {
+            jamesCliHelper = new JamesCliHelper(this);
+        }
+        return jamesCliHelper;
     }
 
     public String property(String name) {
