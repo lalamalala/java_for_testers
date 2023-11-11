fdfbd760dcaa2bd95bd28daefc892896dba9fa5d
Anna <ospostpost9@gmail.com>
2023-11-11 10:52:10 +0100

    Лекция 9.3. Почтовый сервис DeveloperMail.com: регистрация адреса

--- mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrationTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrationTests.java
@@ -1,38 +1,46 @@
 package ru.stqa.mantis.tests;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.stqa.mantis.common.CommonFunctions;
+import ru.stqa.mantis.model.DeveloperMailUser;
 
 import java.time.Duration;
 import java.util.regex.Pattern;
 
 public class UserRegistrationTests extends TestBase{
+
+    DeveloperMailUser user;
     @Test
     void canRegisterUser() throws InterruptedException {
-        var username = String.format(CommonFunctions.randomString(8));
-        var email = String.format("%s@localhost", username);
-       //create user on post server
-  //     app.jamesCli().addUser(email, "password");
-        app.jamesApi().addUser(email, "password");
-       app.session().signup(username, email);
+        var password = "password";
+        user =  app.developerMail().addUser();
+        var email = String.format("%s@developermail.com", user.name());
 
-        var messages = app.mail().receive(email, "password", Duration.ofSeconds(10));
-        var text = messages.get(0).content();
-        var pattern = Pattern.compile("http://\\S*");
-        var matcher = pattern.matcher(text);
-
-        var url = "";
-        if (matcher.find()) {
-            url = text.substring(matcher.start(),matcher.end());
-            System.out.println(url);
-        }
-        app.session().finishedRegistration(url, username, "password");
-
-        Assertions.assertTrue(app.session().isLoggedIn());
-
-        app.http().login(username, "password");
-        Assertions.assertTrue(app.http().isLoggedIn());
+//       app.session().signup(username, email);
+//
+//        var messages = app.mail().receive(email, password, Duration.ofSeconds(10));
+//        var text = messages.get(0).content();
+//        var pattern = Pattern.compile("http://\\S*");
+//        var matcher = pattern.matcher(text);
+//
+//        var url = "";
+//        if (matcher.find()) {
+//            url = text.substring(matcher.start(),matcher.end());
+//            System.out.println(url);
+//        }
+//        app.session().finishedRegistration(url, username, password);
+//
+//        Assertions.assertTrue(app.session().isLoggedIn());
+//
+//        app.http().login(username, password);
+//        Assertions.assertTrue(app.http().isLoggedIn());
         
+    }
+
+    @AfterEach
+    void deleteMailUser () {
+        app.developerMail().deleteUser(user);
     }
 }
